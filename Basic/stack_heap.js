385a5c9e8223ebd0bd1dc534f37f506780c18c6f
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-06 07:08:48 +0000

    stack heap

---
+++ Basic/stack_heap.js
@@ -0,0 +1,6 @@
+//stack (primative datatype)  ,heap (non-primative datatype)
+
+
+//stack gives is copy value to another  variable
+
+//heap gives refrance to another variable which act like og value
