5d6c78800fed5ea6b7912c9057097e6b57501780
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-06 11:45:08 +0000

    array

---
+++ Basic2/array.js
@@ -0,0 +1,36 @@
+let arr = [1,2,3,4,5];   //Array syntx
+
+// let arr2 = new Array(1,2,3,4,5); //also written as 
+
+//array is collection of data of same datatype. which is writen in [] and values inside are called elements
+
+//-------------Array method------------
+
+console.log(arr.push(10)); //add element to last position
+
+console.log(arr.pop()); // remove element from last position of Array
+
+console.log(arr.unshift(0)); // add element from first position of Array
+
+console.log(arr.shift()); // remove element from first position of Array
+
+console.log(arr.includes(9)); //return boolean value check value
+
+let arryMany = [1,2,3,[4,5,6],7,8,9,[0]]
+
+console.log(arryMany.flat(Infinity));
+
+//----------------------------------  
+
+console.log(Array.isArray("laukik"));  //check given string value is array and return boolean value
+
+console.log(Array.from("laukik")); //convert string value to array 
+
+let a = 100;
+let b = 200;
+let c = 300;
+
+console.log(Array.of(a,b,c)); //convert collection of variable in an array
+
+
+
