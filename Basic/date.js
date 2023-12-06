35954dcb4c4baababdb8de03d65eb9f0ed39b222
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-06 10:48:04 +0000

    Date

---
+++ Basic/date.js
@@ -0,0 +1,12 @@
+let myDate = new Date();
+
+console.log(myDate.toString()); // convert date into string maanner
+
+console.log(myDate.toDateString()); //give date without time (Wed Dec 06 2023)
+
+console.log(myDate.toLocaleDateString()); //gives date (12/6/2023)
+
+console.log(myDate.getFullYear()); //get year only 
+
+console.log(myDate.getDay()); //get day only
+
