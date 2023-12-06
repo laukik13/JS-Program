3f1652b3f7b32d7398850a4009c504d810ad62b2
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-06 08:09:07 +0000

    String value

---
+++ Basic/string.js
@@ -0,0 +1,16 @@
+// let textNew = " hello "
+
+//Advance declration
+let textNew = new String(" hello ");
+
+
+//String method
+textNew.trim(" ");
+
+textNew.substring(0,4);
+
+textNew.toUpperCase();
+
+textNew.toLowerCase();
+
+textNew.splice(0,4);
