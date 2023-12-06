a18bb0d210fc57edd7cb46294859fdefb6384100
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-06 06:06:27 +0000

    typeConversion

---
+++ Basic/typeConversion.js
@@ -0,0 +1,18 @@
+let stringValue = "33"
+
+let numberValue = Number(stringValue);
+console.log(numberValue); // 33
+console.log(typeof numberValue); //number
+
+//To connvert string to Number use 
+//Number()
+
+//To connvert Number to string use 
+//String()
+
+//To connvert Number to boolean use 
+//Boolean()
+
+//In boolean
+//1 => true , 0 => false
+//"" => false , "hello" => true
