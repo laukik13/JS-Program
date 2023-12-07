9109768adc49d1a9aa66ed14171380d4d85863f2
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-07 13:08:58 +0000

    control flow

---
+++ Basic3/controlFlow.js
@@ -0,0 +1,42 @@
+//--------if-------
+// if (condition) {
+    
+// }
+
+//-------if else------
+// if (condition) {
+    
+// } else {
+    
+// }
+
+//------switch case-------
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//Nullish coalescing operator (??): null undefined   (only for null and undefined) 
+
+let val1;
+
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+
+
+//and get value which assign first 
+
+
+//--------Terniary Opreator-------
+
+//condition ? true : false
+
+let num = 50;
+
+num > 100 ? console.log("greater") : console.log("lesser")
---
+++ Basic3/truthy_falsy_Value.js
@@ -0,0 +1,16 @@
+const nameValue = "laukik";
+
+if (nameValue) {
+    console.log("I got my name");
+} else {
+    console.log("No name")
+}
+
+//false value
+
+// false, 0, -0, BigInt 0n, "" , null , undefined , NaN
+
+
+//truthy value
+
+// "0", 'false', " " , [], {} , function(){}
