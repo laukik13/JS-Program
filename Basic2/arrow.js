c7d54f02397fe5e31b124ef861f90982bac58162
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-07 07:34:45 +0000

    arrow function

---
+++ Basic2/arrow.js
@@ -0,0 +1,19 @@
+// this // current context  
+
+// this keyword is use to access current value 
+// It is use only in object and not in function
+
+//----------------explist return-------------
+
+const result = (num1,num2) => {
+    return num1 + num2
+}
+
+// when you use {} in function you need  return value to fetch that return is called explist return
+
+
+//----------------implist return------------ 
+
+const result2 = (num1,num2) => (num1 + num2);
+
+// when you not use {} in function and use () you need no return (keyword) value to fetch that return is called implist return
