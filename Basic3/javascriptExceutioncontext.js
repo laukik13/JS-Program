6a3f83e33ed84d18e60f5f8d2a06720fe9fb710b
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-07 11:38:30 +0000

    javascript exceution context

---
+++ Basic3/javascriptExceutioncontext.js
@@ -0,0 +1,35 @@
+//----------javascript Exceution Context---------------
+
+
+//Global Exceution context
+
+//Function Exceution context
+
+//Eval Exceution contaxt
+
+
+
+ // 1 Global Exceution context --- // it will complusory execut
+
+ // 2 Memory creation Phase --- // collect all variable and store in MCP
+
+ // 3 Exceution Phase  --- exceut value to variable / perform exceution
+
+//in exceution context if we found functiont than it will create sperate exceution context  content  (new variable environment) + (exection thread)
+
+
+
+ //call stack
+
+//  call stack work as (LIFO) last in first out
+
+
+ //                     |_________|
+ //                     |_________|
+ //                     |_________|
+ //                     |__Three__|
+ //                     |___Two___|
+ //                     |___one___|
+ //                     |___GEC___|
+
+//  as three() is in top postion(last) and bottom position(first) is (global exceution context)  so after completing its task it will remove top position an than another element
