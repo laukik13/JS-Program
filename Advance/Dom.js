215d52cf842055348b6c595b3b0cefce2a84fa75
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-09 09:57:49 +0000

    DOM

---
+++ Advance/Dom.js
@@ -0,0 +1,13 @@
+//DOM - Document Object Module
+
+  
+
+
+//get value
+
+title.innerContext();   //it get all value from tag if some text is visible or not visible
+
+title.innerText(); //it get value which is visible in html page 
+
+title.innerHTML();  //can get value from given tag but also get other tag inside it
+
