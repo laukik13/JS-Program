375987b94da5538ba4644ae60ce895f2fe7d46b5
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-11 05:25:26 +0000

    event

---
+++ Advance/event.js
@@ -0,0 +1,12 @@
+//event propogation
+
+//Two type of context in event propogation:-
+//event bubbling
+//event capturing
+
+//it is used to bubble the event  
+
+document.addEventListener("click",(e)=>{
+    e.stopPropagation();   // it is method which is used to stop call of over the bubbling
+    console.log("Hello world");
+},false)
