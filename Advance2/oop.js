0c592ce9f8186f706c92447cd469f41a857bb60b
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-11 13:11:01 +0000

    oop

---
+++ Advance2/oop.js
@@ -0,0 +1,30 @@
+//---oops---
+
+// it is process in object to write code 
+
+//---object---
+
+//its is collection of properties and methods
+//eg;- toLowercase()
+
+//---why use oop---
+
+
+//4 pillars
+
+// Abstaction - to hide details eg:-Fetch()
+// Encapusultion - wrapping data an aceess methode for when you need
+// Inheritance - access details from parent class
+// Polymorphism - poly means (more) and mophism means (types)
+
+
+//---this keyword---
+
+// this represent as current context in object
+
+//---new keyword---
+
+// when you use new keyword it create empty object which is called instance
+// than contructor function is called using new keyword and wrap the arguments give it back
+//  than angrument pass from contructor get injected in this keyword 
+// and return data
