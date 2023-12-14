518e6fcfc0cf342a770aa63f441293f08ff85858
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-14 08:21:08 +0000

    arrow

--- Basic2/arrow.js
+++ Basic2/arrow.js
@@ -3,7 +3,7 @@
 // this keyword is use to access current value 
 // It is use only in object and not in function
 
-//----------------explist return-------------
+//----------------explist return----------------
 
 const result = (num1,num2) => {
     return num1 + num2
@@ -12,7 +12,7 @@ const result = (num1,num2) => {
 // when you use {} in function you need  return value to fetch that return is called explist return
 
 
-//----------------implist return------------ 
+//----------------implist return---------------- 
 
 const result2 = (num1,num2) => (num1 + num2);
 
