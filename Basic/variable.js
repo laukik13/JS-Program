4661e9281e6f3d305e4fb3d19aa13352e665e856
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-14 06:09:33 +0000

    variables

--- Basic/variable.js
+++ Basic/variable.js
@@ -5,6 +5,6 @@ let a  = "hello";
 //let is a block scope variable it can reassign and declare in programe but variable declear inside block scope it can not access outside blockscope 
 
 const uniqueId = 12345;
-//const is also a block scope variable it cannot reassign its value as its constant it candeclare in programe but variable declear inside block scope it can not access outside blockscope 
+//const is also a block scope variable it cannot reassign its value as its constant it can declare in programe but variable declear inside block scope it can not access outside blockscope 
 
 
