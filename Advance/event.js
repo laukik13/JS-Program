5cf2ea471d7a82db854839352a1cf3fd5c618d2e
laukik13 <137861269+laukik13@users.noreply.github.com>
2023-12-15 07:22:11 +0000

    event

--- Advance/event.js
+++ Advance/event.js
@@ -10,3 +10,9 @@ document.addEventListener("click",(e)=>{
     e.stopPropagation();   // it is method which is used to stop call of over the bubbling
     console.log("Hello world");
 },false)
+
+//it is event capturing 
+
+document.addEventListener("click",()=>{
+    console.log("Hello world");
+},true)
